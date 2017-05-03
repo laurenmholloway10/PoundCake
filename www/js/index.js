e1bfe11055d2ebdb4d29991d2dce1bb05fad359d
LaurenHolloway <laurenmholloway10@gmail.com>
2017-05-03 10:15:43 -0400

    added other needed pages and updated the Create Challenges fields

--- www/js/index.js
+++ www/js/index.js
@@ -101,40 +101,107 @@ function WriteNewPlayer(){
     document.getElementById("registerForm").reset();
 }
 
-//Work on this, tapped out!
-function viewAllUsers(){
-    for (var newUser in poundcakeDatabase.ref('players')) {
-        newUser = document.createElement("INPUT");
-        newUser.setAttribute("type", "checkbox");
-        var text = document.createTextNode(newUser.firstName);
-        newUser.appendChild(text);
-    }
+//Sign in the user if the user is registered
+function userSignIn(){
+    var email = document.getElementById('Lemail').value;
+    var password = document.getElementById('Lpassword').value;
+firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode === 'auth/wrong-password') {
+    alert('Wrong password.');
+  } else {
+    alert(errorMessage);
+  }
+  console.log(error);
+});
+var user = firebase.auth().currentUser;
 
 }
 //If a user is not signed in, then navigate to the login screen
-var user = firebase.auth().currentUser;
 
-if (user) {
+
+/*while(!user){
+    location.assign("login.html");
+    break;
+}*/
+/*if (user) {
   // User is signed in.
 } else {
   location.assign("login.html");
-}
+
+}*/
 
 //Invite Competitors (max 3)
-function chooseCompetitors(){
-
+/*function getAllUsers(){
+    var allUsers = poundcakeDatabase.ref('players/');
+    return allUsers.once('value').then(function(snapshot) {
+        var userFirst = snapshot.val().firstName;
+        var userLast = snapshot.val().lastName;
+        var userFullName = userFirst + " " + userLast;
+        for (var player in allUsers) {
+            alert(userFullName);
+        }
+    });
+}*/
+function userSignOut(){
+    firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  alert("Whoops...My bad!");
+});
 }
+
+//function to redirect user to accept or decline challenge screen
+var challenges = poundcakeDatabase.ref("challenges");
+var Hostuser = firebase.auth().currentUser;
+if(Hostuser != null){
+    challenges.once("value")
+      .then(function(snapshot) {
+        var challenger = snapshot.val();
+        if(Hostuser.email == challenger.player1){
+            location.assign("InviteFriends.html");
+        }
+        else if (Hostuser.email == challenger.player2){
+            location.assign("InviteFriends.html");
+        }
+        else if (Hostuser.email == challenger.player3){
+            location.assign("InviteFriends.html");
+        }
+        else if (Hostuser.email == challenger.hostPlayer) {
+            location.assign("waitingOnInvites.html");
+        }
+        else {
+            //Do Nothing
+            alert("New friends, new money!");
+        }
+      });
+}
+
 
 function writeNewChallenge() {
     var challengeName = document.getElementById("challengeName").value;
     var buyIn = document.getElementById("buyIn").value;
     var duration = document.getElementById("weeksDuration").value;
     var weeklyPounds = document.getElementById("lbsPerWeek").value;
+    var opponent1 = document.getElementById("opp1").value;
+    var opponent2 = document.getElementById("opp2").value;
+    var opponent3 = document.getElementById("opp3").value;
+
+    var user = firebase.auth().currentUser;
 
   poundcakeDatabase.ref('challenges/').push().set({
     name: challengeName,
     amount: buyIn,
     duration : duration,
-    goal:weeklyPounds
+    goal: weeklyPounds,
+    hostPlayer: user.email,
+    player1: opponent1,
+    player2: opponent2,
+    player3: opponent3,
+    status: false
   });
 }
